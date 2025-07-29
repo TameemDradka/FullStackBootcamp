32d03ce0fe092a0f3c3545c2381bb2810f11ae0f
Tameem Al Daradkah <tameemdaradkah@Mac.attlocal.net>
2025-07-29 11:38:57 -0700

    Added route handlers for navigating through pages

--- Blog Website/index.js
+++ Blog Website/index.js
@@ -8,6 +8,22 @@ app.get("/", (req, res) => {
     res.render("home.ejs");
 });
 
+app.get("/edit", (req, res) => {
+  res.render("edit.ejs");
+});
+
+app.get("/create", (req, res) => {
+  res.render("create.ejs");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about.ejs");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs");
+});
+
 app.listen(port, () => {
     console.log(`Website running on port ${port}`)
 });
