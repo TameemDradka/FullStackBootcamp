2c480949815f69b9acdcc847389a47cee51503d8
Tameem Al Daradkah <tameemdaradkah@Mac.attlocal.net>
2025-07-29 16:32:30 -0700

    Finished edit blog functionality of the website

--- Blog Website/index.js
+++ Blog Website/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import fs from "fs";
+import bodyParser from "body-parser";
 const posts = JSON.parse(fs.readFileSync("posts.json", "utf-8"));
 
 const app = express();
 const port = 3000;
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.render("home.ejs", { posts });
@@ -31,6 +33,22 @@ app.get("/edit/:slug", (req, res) => {
       res.render("editBlog.ejs", { post });
     } else {
       res.status(404).send("Post not found");
+    }
+});
+
+app.post("/confirmEdit/:slug", (req, res) => {
+    const slug = req.params.slug;
+    const updatedTitle = req.body.title;
+    const updatedContent = req.body.content;
+
+    const post = posts.find((p) => p.slug === slug);
+    if (post) {
+        post.title = updatedTitle;
+        post.content = updatedContent;
+
+        res.redirect(`/posts/${slug}`);
+    } else {
+        res.status(404).send("Post not found");
     }
 });
 
