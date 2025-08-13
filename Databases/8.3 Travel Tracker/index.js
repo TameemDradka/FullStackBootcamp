0d442f1e92588dd690690cd477ef31659cad0b04
Tameem Al Daradkah <tameemdaradkah@Mac.attlocal.net>
2025-08-13 16:34:59 -0700

    Finished first part to Travel Tracker app

--- Databases/8.3 Travel Tracker/index.js
+++ Databases/8.3 Travel Tracker/index.js
@@ -1,15 +1,38 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
 
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "Baba2005",
+  port: 5432,
+});
+
+db.connect();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+let visited = [];
+
 app.get("/", async (req, res) => {
   //Write your code here.
+  const result = await db.query("SELECT country_code FROM visited_countries");
+  result.rows.forEach((country) => {
+    visited.push(country.country_code);
+  });
+
+  console.log(visited);
+
+  res.render("index.ejs", { total: visited.length, countries: visited });
+  db.end();
 });
+
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
