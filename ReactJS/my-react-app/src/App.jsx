f70cbc2d62e74a8704d387e0cbd849c9d73b91d0
Tameem Al Daradkah <tameemdaradkah@Mac.attlocal.net>
2025-09-08 12:38:11 -0700

    Finished first 8 lessons of React module that talk about basics, components and structure

--- ReactJS/my-react-app/src/App.jsx
+++ ReactJS/my-react-app/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from "./header";
+import Footer from "./footer";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const fName = "Tameem";
+  const lName = "D";
 
   return (
     <>
+      <Header />
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src="https://fishandmeat.hk/wp-content/uploads/2025/05/arabic-chicken-shawarma-sandwich-recipe-1747792750.jpg" alt="yo"/>
+        <img src="https://images.squarespace-cdn.com/content/v1/5fee3b088500a82fe9d47ac9/1609900284373-K62CMOSW8RIBRS2ZB962/mansaf.jpg" alt="no"/>
+        <img src="https://cdn.prod.website-files.com/62e81a6c2171bd40143e605c/66e83b6c83ebb283250e9ee5_08415-5%20WebKiosk_App_FW_3PcBIC_45Deg_ALC_2000x1333_RAS_CR.jpg" alt="ll"/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <p className="text">Created By {fName} {lName}</p>
+      <p className="text">Copyright 2025.</p>
+
+    
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ ReactJS/my-react-app/src/footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <>
+            <h1>Hello this is footer</h1>
+        </>
+    );
+}
+
+export default Footer;
