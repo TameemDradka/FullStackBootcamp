342afc3e54c4080c012e31ab36c3fc76871a56a2
Tameem Al Daradkah <tameemdaradkah@Mac.attlocal.net>
2025-09-08 21:08:25 -0700

    Finished first part of keeper app project

--- ReactJS/keeper-app/src/components/App.jsx
+++ ReactJS/keeper-app/src/components/App.jsx
@@ -1,5 +1,15 @@
+import Header from "./header"
+import Footer from "./footer"
+import Note from "./note"
+
 function App() {
-  
+  return (
+    <>
+        <Header />
+        <Note />
+        <Footer />
+    </>
+  );
 }
 
 export default App
