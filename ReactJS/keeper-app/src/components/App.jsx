76d54b6d3064ab58bd6f155cc05a78c8e63ee237
Tameem A <tameem.daradkah@gmail.com>
2026-01-12 14:31:29 -0700

    Added new files for react props practice video

--- ReactJS/keeper-app/src/components/App.jsx
+++ ReactJS/keeper-app/src/components/App.jsx
@@ -6,10 +6,7 @@ function App() {
   return (
     <>
         <Header />
-        <Note title="Buy Biz Cards" body="Check different options"/>
-        <Note title="Call Mercy Care" body="Call that bryan dude"/>
-        <Note title="Watch 334 vids" body="Watch the really long vids"/>
-        <Note title="Tell gabriel abt padel" body="show him the 2 options"/>
+        
         <Footer />
     </>
   );
